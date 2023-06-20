590a9480a87713924195fd0125a2618dba3b3008
Leofruck <128100416+Leofruck@users.noreply.github.com>
2023-06-20 19:31:08 -0300

    Update autor_rotas.js

--- rotas/autor_rotas.js
+++ rotas/autor_rotas.js
@@ -9,6 +9,6 @@ router.get('/', autorController.listarAutores);
 router.get('/:id', autorController.buscarAutorPorId);
 router.post('/', autorController.inserirAutor);
 router.put('/:id', autorController.atualizarAutor);
-router.delete('/:id', autorController.deletarAutor);
+router.delete('/delete/:id', autorController.deletarAutor);
 
 module.exports = router;
