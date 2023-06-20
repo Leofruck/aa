feecd13d1bdd944447afac3fc6f015eaeb0abae8
Leofruck <128100416+Leofruck@users.noreply.github.com>
2023-06-20 19:30:37 -0300

    Update livro_rotas.js

--- rotas/livro_rotas.js
+++ rotas/livro_rotas.js
@@ -11,9 +11,13 @@ router.get('/:id', livroController.buscarLivroPorId);
 router.post('/', livroController.inserirLivro);
 router.put('/:id', livroController.atualizarLivro);
 router.delete('/:id', livroController.deletarLivro);
+router.put('/:id/retirar/:clienteId', livroController.retirarLivro);
+router.put('/:id/devolver/:clienteId', livroController.devolverLivro);
 
 
-router.get('/:id/autor', livroController.buscarAutorDoLivro);
+router.get('/autor/:autor', livroController.buscarLivrosPorAutor);
+router.get('/nome/:nome', livroController.buscarLivrosPorNome);
+router.get('/disponiveis/listar', livroController.buscarLivrosDisponiveis);
 router.post('/:id/autor', autorController.associarAutorAoLivro);
 router.delete('/:id/autor', autorController.desassociarAutorDoLivro);
 
